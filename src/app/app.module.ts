20d614d0367d04cc9fe75f6328c46d1fa993b8a0
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-12-18 15:54:51 +0200

    create reg form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RegFormModule } from './reg-form/reg-form.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RegFormModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
